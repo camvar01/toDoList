0434b781114875a2ac46583a3b5aee85b3580df6
camvar01 <milo.avc@gmail.com>
2025-10-26 21:59:45 -0500

    estilos con bs5

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,37 +1,97 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplicación To Do List</title>
+    <head>
+        <title>Aplicación con laravel</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
 
-</head>
-<body>
-    <form action="{{ url('/') }}" method="post">
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main class="container">
+        
+        <br/>
+        <div class="card">
+            <div class="card-header">Tareas:</div>
+            <div class="card-body">
+
+        <form action="{{ url('/') }}" method="post">
         @csrf
-        <input type="text" name="task" id="task" placeholder="Escribe una tarea">
-        <input type="submit" value="Agregar tarea">
+        Tarea: 
+        <br/><br/>
+        <input type="text" class="form-control" name="task" id="task" placeholder="Escribe una tarea">
+        <br/>
+        <input type="submit" class="btn btn-primary" value="Agregar tarea">
        
-    </form>
-<br>
-    <table border="1">
-        <tr>
-            <th>Nombre de la tarea</th>
-            <th>Acción</th>
-        </tr>
-
-    @foreach ($tasks as $task)
-
-        <tr>
-            <td>{{ $task->task }}</td>
-            <td>
-                <form action="{{ route('task.destroy', $task->id) }}" method="post">
+        </form>
+        <br/>
+        <div
+            class="table-responsive-sm table-bordered table-striped"
+        >
+            <table
+                class="table"
+            >
+                <thead>
+                    <tr>
+                        <th scope="col">Acción</th>
+                        <th scope="col">Nombre de la tarea</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($tasks as $task)
+                    <tr class="">
+                        <td>
+                        <form action="{{ route('task.destroy', $task->id) }}" method="post">
                     @csrf
                     @method('DELETE')
-                    <input type="submit" value="x">
-                </form>
-            </td>
-        </tr>
-    @endforeach
-</body>
+                    <input type="submit" class="btn btn-danger" value="(x) Eliminar">
+                    </form>
+                    </td>
+                        <td>{{ $task->task }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        
+
+            </div>
+            <div class="card-footer text-muted"></div>
+        </div>
+
+        <br/>
+
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
+
+
